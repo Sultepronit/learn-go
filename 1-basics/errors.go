84698911ab88d9daad922bf1f305116ac435b236
Sultepronit <sultepronit@gmail.com>
2025-08-13 19:26:31 +0300

    errors are studied

--- 1-basics/errors.go
+++ 1-basics/errors.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 // By convention, errors are the last return value and have type error, a built-in interface.
 func f(arg int) (int, error) {
 	if arg == 42 {
@@ -18,17 +23,32 @@ func makeTea(arg int) error {
 	if arg == 2 {
 		return ErrOutOfTea
 	} else if arg == 4 {
-		retrun fmt.Errorf("making tea: %w", ErrPower)
+		return fmt.Errorf("making tea: %w", ErrPower)
 	}
 	return nil
 }
 
 func main() {
-	for _, i := f(i) range []int{7, 42} {
+	for _, i := range []int{7, 42} {
 		if r, e := f(i); e != nil {
 			fmt.Println("f failed:", e)
 		} else {
 			fmt.Println("f worked:", r)
 		}
+	}
+
+	for i := range 5 {
+		if err := makeTea(i); err != nil {
+			if errors.Is(err, ErrOutOfTea) {
+				fmt.Println("We should buy some tea!")
+			} else if errors.Is(err, ErrPower) {
+				fmt.Println("It's dark!")
+			} else {
+				fmt.Printf("Unknown error: %s\n", err)
+			}
+			continue
+		}
+
+		fmt.Println("Tea is ready!")
 	}
 }
