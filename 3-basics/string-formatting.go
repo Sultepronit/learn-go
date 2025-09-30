345d5c30b8d2019c102467157f9d811bbfee18eb
Sultepronit <sultepronit@gmail.com>
2025-09-30 16:31:50 +0300

    finished the String Formatting lesson

--- 3-basics/string-formatting.go
+++ 3-basics/string-formatting.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // Go offers excellent support for string formatting in the printf tradition.
 
@@ -25,6 +28,7 @@ func main() {
 	pf("string: %s\n", s)      // string: "string"        A
 	pf("string: %q\n", s)      // string: string: "\"string\"\tA"
 	pf("bool: %t\n", true)     // bool: true
+	pf("pointer: %p\n", &p)    // pointer: 0xc000088020
 	pf("int (deс): %d\n", 598) // int (deс): 598
 	pf("bin: %b\n", 15)        // bin: 1111
 	pf("hex: %x\n", 100)       // hex: 64
@@ -32,6 +36,18 @@ func main() {
 	pf("sci: %e\n", 3.14)      // sci: 3.140000e+00
 	pf("sci: %E\n", 3.14)      // sci: 3.140000E+00
 
-	pf("char: %c\n", 33) // char: !
+	// %x renders the string in base-16, with two output characters per byte of input.
+	pf("string->hex: %x\n", s) // 22737472696e67220941
+	pf("char: %c\n", 33)       // char: !
 
+	pf("|%6d|%6d|\n", 11, 256)            // |    11|   256|
+	pf("|%6.2f|%6.2f|\n", 10.0, 13.256)   // | 10.00| 13.26|
+	pf("|%-6d|%-6.2f|\n", 7, 3.145)       // |7     |3.15  |
+	pf("|%6s|%6s|\n", "hey world", "app") // |hey world|   app|
+	pf("|%-6s|%-6s|\n", "apple", "pie")   // |apple |pie   |
+
+	str := fmt.Sprintf("%9s", "go!")
+	fmt.Println(str) //       go!
+
+	fmt.Fprintf(os.Stderr, "Error: %s\n", "Something went wrong!") // Error: Something went wrong!
 }
