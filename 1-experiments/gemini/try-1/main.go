40c30eb3e35ce75503c2a00ddc17e6b0b10dfe49
sultepronit <sultepronit@gmail.com>
2025-12-23 11:37:07 +0000

    ok, it works somehow

--- 1-experiments/gemini/try-1/main.go
+++ 1-experiments/gemini/try-1/main.go
@@ -3,20 +3,31 @@ package main
 // go get google.golang.org/genai
 
 import (
-    "context"
-    "fmt"
-    "log"
+	"context"
+	"fmt"
+	"log"
 
-    "google.golang.org/genai"
+	"google.golang.org/genai"
 )
 
 func main() {
-    ctx := context.Background()
-    client, err := genai.NewClient(ctx, &genai.ClientConfig{
-		HTTPOptions: genai.HTTPOptions{APIVersion: "v1"},
-	})
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(client)
+	ctx := context.Background()
+    // The client gets the API key from the environment variable `GEMINI_API_KEY`.
+	client, err := genai.NewClient(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	model := "gemini-2.5-flash-lite"
+	result, err := client.Models.GenerateContent(
+		ctx,
+		model,
+		genai.Text("One-sentence joke, please."),
+		nil,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result.Text())
 }
